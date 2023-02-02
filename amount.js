494749d8fb91977f005b204956cc8d6eaf90c8c2
muhammadriazanus <riazanus568@gmail.com>
2023-02-02 01:17:52 -0800

    first commit

---
+++ amount.js
@@ -0,0 +1,22 @@
+// var originalNumber = "compound"
+// variable doesnot start in number 
+// camel case use in javascript and snake case use in java script
+// varible an illegal variable familier operator 
+// variable which has the numerical area icrement kar 
+// pre incerement ++num
+// post increment num++
+// defined means value doesnot assign variable
+// var num = 2;
+// var total = num++ + --num + ++num - num--;
+// console.log(++num)
+// var a = 2;
+// var b = 1;
+// var result = --a  - --b + ++b + b--;
+// alert(result) 
+// var num = 2* 4 * 4 + 2;
+// alert(num)
+var firstName = "muhammad ";
+var lastName = "anas";
+var fullName = firstName + " " + lastName;
+// alert("hello" + " " + fullName )
+document.write("<h1>" + fullName + "</h1>"")
--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-// var a ="test";
-// console.log(a)
-// document.write("<h1>muhammad <br/> anas</h1>");
-// var fristName = prompt("what is your firstName??");
-// alert(fristName)
-// var a = "test";
-// console.log(a)
-// var a = "test";
-// console.log(a)
-// var nationality = "american";
-// console.log(nationality)
-// var saylaniWelfare = "amount";
-// console.log(saylaniWelfare)
-var a = "I dont know"
+// // var a ="test";
+// // console.log(a)
+// // document.write("<h1>muhammad <br/> anas</h1>");
+// // var fristName = prompt("what is your firstName??");
+// // alert(fristName)
+// // var a = "test";
+// // console.log(a)
+// // var a = "test";
+// // console.log(a)
+// // var nationality = "american";
+// // console.log(nationality)
+// // var saylaniWelfare = "amount";
+// // console.log(saylaniWelfare)
+// // var a = "I dont know";
+// // alert(a)
+// // var weight = "150";
+// // alert(weight)
+// // var num1 = 12;
+// // var num2 = 10;
+// // var total = num1 + num2;
+// // // alert(total + 3)
+// // var number ="10" ;
+// // var number = number / 5;
+// // console.log(number)
+// var numericalAreaMeter = 200;
+// alert(numericalAreaMeter)
+// console.log(abc)
+
+// chapter no 5 math expression familier operation
+// var popualrNumber = 10;
+// console.log(popualrNumber)
+// var a = 2;
+// alert(a)
+// var a = 2;
+// alert(a++);
+// var a = 2;
+// alert(++a)
+var num = 2;
+// num++
+// num--
+// pre increment (++num) 
+// post increment (num++)
+alert(++num)
