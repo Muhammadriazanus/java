635daa6dfa2199ed52dafdb28b1ec63114e0f0f8
muhammadriazanus <riazanus568@gmail.com>
2023-02-01 22:50:09 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// var a ="test";
+// console.log(a)
+// document.write("<h1>muhammad <br/> anas</h1>");
+// var fristName = prompt("what is your firstName??");
+// alert(fristName)
+// var a = "test";
+// console.log(a)
+// var a = "test";
+// console.log(a)
+// var nationality = "american";
+// console.log(nationality)
+// var saylaniWelfare = "amount";
+// console.log(saylaniWelfare)
+var a = "I dont know"
